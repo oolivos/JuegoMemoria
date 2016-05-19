24f37d764559fa721ec2faaeda6dd10fffa78ae1
Oscar Javier <ojolivos@gmail.com>
2016-05-18 21:35:02 -0500

    First Commit

---
+++ JuegoMemoria.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+//: Oscar Javier Olivos
+import UIKit
+
+for i in 0...100 {
+    
+        if i % 5 == 0 {
+            print("\(i) Bingo!!! ")
+        }else if i % 2 == 0 {
+            print("\(i) Par!!!")
+        }else if i % 2 != 0 {
+            print("\(i) Impars!!!")
+    }
+    
+    if i > 30 && i < 40 {
+        print("\(i) Viva Swift!!!")
+    }
+        
+    
+}
